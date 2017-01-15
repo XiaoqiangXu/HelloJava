26e066a61c73ade5d551f06531b469ddc588240a
Xiaoqiang Xu <xu.xiaoqiang@qq.com>
2017-01-15 21:42:26 +0800

    weeklearning

---
+++ Java Programming Duke Coursera/Courses 3/Week2/src/CodenCount.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Xiaoqiang on 2017/1/15.
+ */
+public class CodenCount
+{
+
+
+
+}
---
+++ Java Programming Duke Coursera/Courses 3/Week2/src/WordFrequenciesMap.java
@@ -0,0 +1,35 @@
+import edu.duke.*;
+import  java.util.*;
+
+/**
+ * Created by Xiaoqiang on 2017/1/15.
+ */
+public class WordFrequenciesMap {
+    public void countWordsMap(){
+        FileResource resource = new FileResource();
+        int total = 0;
+        HashMap<String,Integer> map=new HashMap<>();
+        for (String w:resource.words()){
+            total++;
+            w = w.toLowerCase();
+            if(!map.containsKey(w)){
+                map.put(w,1);
+            }else {
+                map.put(w,map.get(w)+1);
+            }
+        }
+        for (String w: map.keySet()){
+            int occurrences = map.get(w);
+            if (occurrences>500){
+                System.out.println(occurrences+"  "+w);
+            }
+        }
+        System.out.println("total = "+total);
+
+    }
+    public static void main (String[] arg){
+        WordFrequenciesMap hml = new WordFrequenciesMap();
+        hml.countWordsMap();
+    }
+}
+
