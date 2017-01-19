10ac407e8e397348fed0ed6d4d609d39a85c5703
Xiaoqiang Xu <xu.xiaoqiang@qq.com>
2017-01-19 21:33:00 +0800

    week3-2/3

--- Java Programming Duke Coursera/Courses 3/Week3/src/LogAnalyzer.java
+++ Java Programming Duke Coursera/Courses 3/Week3/src/LogAnalyzer.java
@@ -25,8 +25,61 @@ public class LogAnalyzer
              records.add(WebLogParser.parseEntry(line));
          }
      }
-        
-     public void printAll() {
+
+     public int countUniqueIPs(){
+         ArrayList<String> uniqueIPs = new ArrayList<>();
+         for (LogEntry le:records){
+             String ipAddr = le.getIpAddress();
+             if (!uniqueIPs.contains(ipAddr)){
+                 uniqueIPs.add(ipAddr);
+             }
+         }
+         return  uniqueIPs.size();
+     }
+
+     public void printAllHigherThanNum(int num){
+         for(LogEntry le:records){
+             int statusCode = le.getStatusCode();
+             if (statusCode>num){
+                 System.out.println(le);
+             }
+         }
+     }
+
+     public ArrayList<String> uniqueIPVisitsOnDay(String someday){
+         ArrayList<String> IPsOnDay = new ArrayList<>();
+         for (LogEntry le:records){
+             String str = le.getAccessTime().toString();
+             int idx = str.indexOf(someday);
+             if (idx!=-1){
+                 if (!IPsOnDay.contains(le.getIpAddress())) {
+                     IPsOnDay.add(le.getIpAddress());
+                 }
+             }
+
+         }
+         return IPsOnDay;
+     }
+
+     public int countUniqueIPsInRange(int low, int high){
+         ArrayList<String> uniqueIPsInRange = new ArrayList<>();
+         for (LogEntry le:records){
+             if (le.getStatusCode()>=low&&le.getStatusCode()<=high){
+                 if (!uniqueIPsInRange.contains(le.getIpAddress())){
+                     uniqueIPsInRange.add(le.getIpAddress());
+                 }
+
+             }
+         }
+         return uniqueIPsInRange.size();
+
+     }
+
+
+
+
+
+    public void printAll() {
          for (LogEntry le : records) {
              System.out.println(le);
          }
--- Java Programming Duke Coursera/Courses 3/Week3/src/Tester.java
+++ Java Programming Duke Coursera/Courses 3/Week3/src/Tester.java
@@ -6,6 +6,8 @@
  * @version (a version number or a date)
  */
 
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
 import java.util.*;
 
 public class Tester
@@ -18,11 +20,19 @@ public class Tester
     }
     
     public static void main(String[] args) {
-//        Tester ts = new Tester();
-//        ts.testLogEntry();
         LogAnalyzer la = new LogAnalyzer();
-        la.readFile("short-test_log");
-        la.printAll();
+        la.readFile("weblog1_log");
+//        la.printAll();
+        System.out.println("There are "+la.countUniqueIPs()+" IPs.");
+
+        la.printAllHigherThanNum(400);
+
+        ArrayList<String> IPsOnday = la.uniqueIPVisitsOnDay("Mar 17");
+        System.out.println(IPsOnday.size()+" IPs:"+IPsOnday);
+
+
+        System.out.println(la.countUniqueIPsInRange(300,399));
+
 
     }
 }
