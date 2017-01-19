65a91a199dbf9ef456ac7429c60634d3a72f7891
Xiaoqiang Xu <xu.xiaoqiang@qq.com>
2017-01-20 00:23:33 +0800

    week3end

--- Java Programming Duke Coursera/Courses 3/Week3/src/LogAnalyzer.java
+++ Java Programming Duke Coursera/Courses 3/Week3/src/LogAnalyzer.java
@@ -72,16 +72,100 @@ public class LogAnalyzer
              }
          }
          return uniqueIPsInRange.size();
-
      }
-
-
-
-
 
     public void printAll() {
          for (LogEntry le : records) {
              System.out.println(le);
          }
      }
+
+     public HashMap<String,Integer> countVisitsPerIP(){
+         HashMap<String,Integer> counts = new HashMap<>();
+         for (LogEntry le:records){
+             if (!counts.containsKey(le.getIpAddress())){
+                 counts.put(le.getIpAddress(),1);
+             }else{
+                 counts.put(le.getIpAddress(),counts.get(le.getIpAddress())+1);
+             }
+         }
+         return counts;
+     }
+
+    public int mostNumberVisitsByIP(HashMap<String,Integer> counts){
+         int mostNVBIP = 0;
+         for (int temp:counts.values()){
+             if (temp>mostNVBIP){
+                 mostNVBIP = temp;
+             }
+         }
+         return mostNVBIP;
+    }
+
+    public ArrayList<String> iPsMostVisits(HashMap<String,Integer> counts){
+        int mostNVBIP = mostNumberVisitsByIP(counts);
+        ArrayList<String> al = new ArrayList<>();
+        for (String key: counts.keySet()){
+            if (counts.get(key)==mostNVBIP){
+                al.add(key);
+            }
+        }
+        return al;
+
+    }
+
+    public HashMap<String,ArrayList<String>> iPsForDays(){
+        HashMap<String,ArrayList<String>> IPFD = new HashMap<>();
+        for (LogEntry le:records){
+            String str = le.getAccessTime().toString();
+            str = str.substring(4,10);
+            if (!IPFD.containsKey(str)){
+                ArrayList<String> al = new ArrayList<>();
+                al.add(le.getIpAddress());
+                IPFD.put(str,al);
+            }else{
+                ArrayList<String> al = IPFD.get(str);
+                al.add(le.getIpAddress());
+                IPFD.put(str,al);
+            }
+        }
+        return IPFD;
+    }
+
+    public String dayWithMostIPVisits(HashMap<String,ArrayList<String>> IPFD){
+        String output = null;
+        int maxcount = 0;
+        for (String str:IPFD.keySet()){
+            if (IPFD.get(str).size()>maxcount){
+                maxcount = IPFD.get(str).size();
+                output = str;
+            }
+
+        }
+        return output;
+    }
+
+    public ArrayList<String> iPsWithMostVisitsOnDay(HashMap<String,ArrayList<String>> IPFD, String someday){
+        ArrayList<String> al = IPFD.get(someday);
+        ArrayList<String> output = new ArrayList<>();
+        HashMap<String,Integer> IPwithMostVOD= new HashMap<>();
+        for (String str:al){
+            if (!IPwithMostVOD.containsKey(str)){
+                IPwithMostVOD.put(str,1);
+
+            }else{
+                IPwithMostVOD.put(str,IPwithMostVOD.get(str)+1);
+            }
+        }
+        int mostNVBIP = mostNumberVisitsByIP(IPwithMostVOD);
+        for (String str:IPwithMostVOD.keySet()){
+            if (IPwithMostVOD.get(str)==mostNVBIP){
+                output.add(str);
+            }
+        }
+        return output;
+    }
+
+
+
 }
--- Java Programming Duke Coursera/Courses 3/Week3/src/Tester.java
+++ Java Programming Duke Coursera/Courses 3/Week3/src/Tester.java
@@ -6,9 +6,9 @@
  * @version (a version number or a date)
  */
 
-import com.sun.xml.internal.fastinfoset.util.StringArray;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class Tester
 {
@@ -21,17 +21,34 @@ public class Tester
     
     public static void main(String[] args) {
         LogAnalyzer la = new LogAnalyzer();
-        la.readFile("weblog1_log");
+//        la.readFile("weblog1_log");
+        la.readFile("weblog2_log");
+
+        HashMap<String,Integer> counts = la.countVisitsPerIP();
+        System.out.println(counts);
+
+        System.out.println("Maximum number of visits to this website by a single IP address "+la.mostNumberVisitsByIP(counts));
+        System.out.println("Maximum visited IP addresses "+la.iPsMostVisits(counts));
+
+        HashMap<String,ArrayList<String>> IPFD= la.iPsForDays();
+        System.out.println(IPFD);
+
+        System.out.println(la.dayWithMostIPVisits(IPFD));
+
+        System.out.println(la.iPsWithMostVisitsOnDay(IPFD,"Sep 29"));
+
+
+
 //        la.printAll();
         System.out.println("There are "+la.countUniqueIPs()+" IPs.");
-
-        la.printAllHigherThanNum(400);
-
-        ArrayList<String> IPsOnday = la.uniqueIPVisitsOnDay("Mar 17");
+//
+//        la.printAllHigherThanNum(400);
+//
+        ArrayList<String> IPsOnday = la.uniqueIPVisitsOnDay("Sep 27");
         System.out.println(IPsOnday.size()+" IPs:"+IPsOnday);
-
-
-        System.out.println(la.countUniqueIPsInRange(300,399));
+//
+//
+        System.out.println(la.countUniqueIPsInRange(200,299));
 
 
     }
