3296f2e648dee4293f5056f4004750330e56bdec
Xiaoqiang Xu <xu.xiaoqiang@qq.com>
2017-01-21 22:28:46 +0800

    week1start

---
+++ Java Programming Duke Coursera/Courses 4/Week 1/src/tester.java
@@ -0,0 +1,15 @@
+/**
+ * Created by XXQ on 2017/1/21.
+ */
+public class tester {
+    public static void main(String[] args){
+        EarthQuakeClient quakeClient = new EarthQuakeClient();
+//        quakeClient.bigQuakes();
+//        quakeClient.closeToMe();
+        quakeClient.quakesOfDepth();
+
+
+//        ClosestQuakes closestQuakes = new ClosestQuakes();
+//        closestQuakes.findClosestQuakes();
+    }
+}
